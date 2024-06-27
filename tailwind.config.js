d0bc50279dda9d47ecf2211a6595142b64b4b500
Hamza Younis <hamzayounis106@gmail.com>
2024-06-27 17:31:14 +0500

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xm': '480px', // Custom screen size named 'xm'
+      },
+    },
   },
   plugins: [],
 }
